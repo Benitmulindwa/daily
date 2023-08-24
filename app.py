ac48dd20e46976a7532e3ba112036d97193c9761
Replit user <>
2023-08-24 20:44:09 +0000

    some changes

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 from flask import Flask, render_template
-from datetime import datetime
 import requests
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///daily.db'
+db = SQLAlchemy(app)
 
+
+class User(db.Model):
+  __tablename__ = 'users'
+  id = db.Column(db.Integer(), primary_key=True)
+  username = db.Column(db.String(30), nullable=False, unique=True)
+  email = db.Column(db.String(50), nullable=False, unique=True)
+  password = db.Column(db.String(60), nullable=False)
+
+
+with app.app_context():
+  db.create_all()
+  user1 = User(username="john",
+               email="exam@gmail.com",
+               password="123456789")
+  db.session.add(user1)
+  db.session.commit()
+  print(User.query.all())
+  pass
+#print(user1.username)
 
 @app.route("/")
 def home():
