0eb0d077c336cf3e72239c6f49a1a0ddcab5506d
Benitmulindwa <mulindwabenit@gmail.com>
2023-08-27 17:38:58 +0200

    handles the ValidationError

--- daily/forms.py
+++ daily/forms.py
@@ -1,10 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import Length, Email, EqualTo, DataRequired
+from wtforms.validators import Length, Email, EqualTo, DataRequired, ValidationError
+from daily.models import User
 
 
 class RegisterForm(FlaskForm):
 
+    def validate_username(self, username):
+        userfield=User.query.filter_by(username=username.data).first()
+        if userfield:
+            raise ValidationError("Username already exists! Please try a different username")
+
+    def validate_email(self, email):
+        emailfield = User.query.filter_by(email=email.data).first()
+        if emailfield:
+            raise ValidationError("Email already exists! Please try a different email address!")
+        
     username = StringField(label="Username: ", validators=[Length(min=2), DataRequired()])
     email = StringField(label="Email: ", validators=[Email(), DataRequired()])
     password1 = PasswordField(label="Password: ", validators=[Length(min=7), DataRequired()])
